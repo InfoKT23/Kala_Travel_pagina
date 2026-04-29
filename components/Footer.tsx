c8ca5f5f5545c1d7ac8f9c5f73c43f0f04325fda
InfoKT23 <info@kalatravel.tours>
2026-04-29 14:52:23 -0500

    EEROR

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <h3>KALA TRAVEL</h3>
-      <p>Viajes, visas, vuelos y seguros de viaje.</p>
-      <p>© 2026 Kala Travel. Proyecto académico.</p>
-    </footer>
-  );
-}
