0f1199765eff3e5e1f0ab2b872b2b1e9c6bc30e3
InfoKT23 <info@kalatravel.tours>
2026-04-29 17:52:03 -0500

    IMAGEN FOOTER
    LOGO BLANCO

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
 export default function Footer() {
   return (
     <footer>
-      <h3>KALA TRAVEL</h3>
+      <Image 
+  src="/images/logo_blanco.png"
+  alt="Kala Travel"
+  width={160}
+  height={60}
+/>
       <p>Viajes, visas, vuelos y seguros de viaje.</p>
-      <p>© 2026 Kala Travel. Proyecto académico.</p>
+      <p>© 2026 Kala Travel.</p>
     </footer>
   );
 }
